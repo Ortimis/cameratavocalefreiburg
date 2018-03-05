e75be39954b649b490ba3e9fcb69f915a0b543eb
Ortimis <jakob@drortmann.de>
2018-03-05 17:40:45 +0100

    slider changes

--- js/common.js
+++ js/common.js
@@ -384,7 +384,7 @@
             slidesToShow: 1,
             slidesToScroll: 1,
             autoplay: true,
-            autoplaySpeed: 2000,
+            autoplaySpeed: 5000,
             arrows: false,
             dots: true
         });
